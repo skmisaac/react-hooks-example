0d42c1f0a7b33a913929e1566afca26db1e4a813
Isaac Sun <skmisaac@users.noreply.github.com>
2024-07-01 19:08:30 +0800

    initial commit of react-hooks example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([
+    { completed: false, title: "Task #1" },
+    { completed: false, title: "Task #2" },
+    { completed: false, title: "Task #3" },
+  ]);
+
+  const [newItemTitle, setNewItemTitle] = useState("");
+
+  const handler = (e) => {
+    setNewItemTitle(e.target.value);
+  };
+  // const [count, setCount] = useState(20);
+
+  // const countState = useState(20);
+  // const count = countState[0];
+  // const setCount = countState[1];
+
+  const keyDownHandler = ({ key }) => {
+    if (key === "Enter") {
+      const newTodos = [...todos];
+      newTodos.push({ completed: false, title: newItemTitle });
+      setTodos(newTodos);
+      setNewItemTitle("");
+    }
+  };
+
+  const toggle = (index) => {
+    const newTodos = todos.map((todo, todoIndex) => {
+      const isCompleted = todos[todoIndex].completed;
+      return {
+        ...todo,
+        completed: index === todoIndex ? !isCompleted : isCompleted,
+      };
+    });
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <li
+              key={index}
+              onClick={() => {
+                toggle(index);
+              }}
+              className={todo.completed ? "completed" : ""}
+            >
+              {todo.title}
+            </li>
+          );
+        })}
+      </ul>
+      <h2>{newItemTitle}</h2>
+
+      <input
+        type="text"
+        value={newItemTitle}
+        onChange={handler}
+        onKeyDown={keyDownHandler}
+      />
     </div>
   );
 }
---
+++ src/Greeting.js
@@ -0,0 +1,11 @@
+function Greeting({ name, message }) {
+  return (
+    <div>
+      <h2>
+        {name}: {message}
+      </h2>
+    </div>
+  );
+}
+
+export default Greeting;
